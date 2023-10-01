ccf3097ca07932bd265b35311acd6eeadcc976fd
Ken <109365944+OutOfIdeasException@users.noreply.github.com>
2023-10-01 12:31:46 -0400

    Update scripts.js

--- JavaScript-UnixTimeConverter/scripts.js
+++ JavaScript-UnixTimeConverter/scripts.js
@@ -1,13 +1,18 @@
-function convert() {
-// Get the value of the unix time stamp that the user entered
-	var inDate = document.getElementById('inputDate').value;
-// Get the element to display the converted result
-	var outDate = document.getElementById('displayDate');
+function displayCurrentTime() {
 
-// Declare a variable to store the date in milliseconds
-	var mSeconds = parseInt(inDate);
-// Create a new Date object based on the previously delcared variable
-	var date = new Date(mSeconds);
-// Display the date in a string format within the outDate div 
-	outDate.innerHTML = date.toString();
-	}
+  let tDate = document.getElementById('todayDate');
+  const td = new Date().getTime()
+  
+  todayDate.innerHTML = (td + ' (Local)');
+  
+  console.log('Display Button Clicked');
+}
+
+function convert() {
+  var inDate = document.getElementById('inputDate').value;
+  var outDate = document.getElementById('displayDate');
+  var mSeconds = parseInt(inDate);
+  var date = new Date(mSeconds);
+	
+  outDate.innerHTML = date.toString();
+}
